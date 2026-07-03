21cb9191ec8d3f682cb51120be1263b961dde54f
jatin45ppandey-design <jatin45ppandey@gmail.com>
2026-07-03 19:06:59 +0530

    some changes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
-    <div>Footer</div>
+      <div>
+      <section style={{backgroundColor:'black'}} >
+       
+  <ul style={{display:'flex',gap:'150px',padding:'10px',margin:'10px',cursor:'pointer',color:'wheat',listStyle:'none',textDecoration: 'none'}}>
+    
+         <Link to = 'https://github.com/jatin45ppandey-design' style={{textDecoration: 'none'}}>
+          <li style={{color:'wheat'}}>Github</li>
+         </Link>
+         <Link to = 'https://www.linkedin.com/in/jatin-pandey-pandey-a1654237a/' style={{textDecoration: 'none'}}>
+          <li style={{color:'wheat'}}>Linkedin</li>
+         </Link>
+         <Link to = 'https://leetcode.com/u/Jatin45_Pandey/' style={{textDecoration: 'none'}}>
+          <li style={{color:'wheat'}}>Leetcode</li>
+         </Link>
+         <Link to = 'https://www.instagram.com/jatin45p/' style={{textDecoration: 'none'}}>
+            <li style={{color:'wheat'}}>Whatsapp </li>
+
+          </Link> 
+        </ul>
+      </section>
+    </div>
     
   )
 }
